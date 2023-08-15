7576a7e621e6550087e0416ee39cc2fdd1a80ea4
Queentart <96613722+Queentart@users.noreply.github.com>
2023-08-15 17:23:16 +0900

    Update SignUpController.java

--- src/main/java/com/example/project101/controller/SignUpController.java
+++ src/main/java/com/example/project101/controller/SignUpController.java
@@ -1,24 +1,31 @@
 package com.example.project101.controller;
 
+import com.example.project101.dto.MemberTableDTO;
+import com.example.project101.service.MemberTableService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
+@RequiredArgsConstructor
 public class SignUpController {
 
-    // 회원가입 페이지 출력 요청
+    private final MemberTableService memberService;
+
     @GetMapping("/register")
     public String showRegisterPage() {
-        return "register"; // register.html 파일명
+        return "register"; // 회원가입 페이지를 보여줌
     }
 
     @PostMapping("/register")
-    public String save(
-            @RequestParam("name") String name,
-            @RequestParam("email") String email,
-            @RequestParam("password") String password) {
-        System.out.println("SignUpController.save");
-        System.out.println("name = " + name + ", email = " + email + ", password = " + password);
-        return "redirect:/login"; // 회원가입 후 로그인 페이지로 이동
+    public String register(@ModelAttribute MemberTableDTO memberDTO, Model model) {
+        boolean registrationSuccess = memberService.register(memberDTO);
+        if (registrationSuccess) {
+            return "redirect:/login"; // 회원가입 후 로그인 페이지로 이동
+        } else {
+            model.addAttribute("errorMessage", "이미 사용 중인 이메일입니다."); // 에러 메시지 전달
+            return "register"; // 회원가입 실패 시 다시 회원가입 페이지로 이동
+        }
     }
 }
